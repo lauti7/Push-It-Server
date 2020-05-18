bc3484edfd1d3af53a79849cc15674ab5538cdb1
lauti7 <laugerbustos@hotmail.com>
2020-05-17 23:35:27 -0300

    added routes

--- routes/segments.js
+++ routes/segments.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 const isAuthenticated = require('../middlewares/isAuthenticated')
-const {create, appSegments} = require('../controllers/segmentsController')
+const {create, appSegments, show, subscribers, changeStatus} = require('../controllers/segmentsController')
 
-router.get('/:appId', isAuthenticated, appSegments)
+router.get('/:_id', isAuthenticated, show)
+router.get('/:_id/subscribers', isAuthenticated, subscribers)
+router.get('/:_id/status', isAuthenticated, changeStatus)
+router.get('/app/:appId', isAuthenticated, appSegments)
 router.post('/:appId/create', isAuthenticated, create)
+
 
 
 module.exports = router
