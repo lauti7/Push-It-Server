1f3d932be9a63604afd4014add5262e1b5d22d64
lauti7 <laugerbustos@hotmail.com>
2020-05-17 23:31:35 -0300

    changes in login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,25 +1,32 @@
 const User = require('../services/userService')
 const TokenHelper = require('../utils/token')
+const bcrypt = require('bcryptjs')
+
 
 const register = async (req, res) => {
   const user = await User.register(req.body)
 
-  if (!user) {
+  if (user === null) {
     return res.status(500).json({success: false})
   }
 
-  const token = TokenHelper.createUserToken(user._id)
+  if (user === false) {
+    return res.status(200).json({success: false, message: 'Email already exists'})
+  }
+
+  const token = await TokenHelper.createUserToken(user._id)
 
   return res.status(200).json({success: true, token})
 }
 
 const login = async (req, res) => {
   const {email, password} = req.body
-  const user = User.getUser(email)
+  const user = await User.getUser(email)
+  console.log(user);
   if (user) {
     const comparePassword = await bcrypt.compare(password, user.password)
     if (comparePassword) {
-      const token = TokenHelper.createUserToken(user._id)
+      const token = await TokenHelper.createUserToken(user._id)
       return res.status(200).json({success: true, token})
     } else {
       return res.status(500).json({success: false, message: 'Wrong Password'})
