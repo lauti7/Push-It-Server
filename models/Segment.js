50632dacb73946a566a23d31812d0395eba1aaf6
lauti7 <laugerbustos@hotmail.com>
2020-05-17 23:32:33 -0300

    added condition, status, updatedAt fields

--- models/Segment.js
+++ models/Segment.js
@@ -4,8 +4,10 @@ const Schema = mongoose.Schema;
 const segmentSchema = new Schema({
   name: {type: String},
   appId: {type: mongoose.Types.ObjectId, ref: 'Application'},
-  match: {type: String},
+  condition: {type: Array},
   count: {type: Number},
+  status: {type: String, default: 'enable'},
+  updatedAt: {type: Date, default: Date.now}
 }, {collection: 'Segments'})
 
 const Segment = mongoose.model('Segment', segmentSchema)
